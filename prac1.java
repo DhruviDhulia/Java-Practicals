24f793c4be661825794823ee8d75cbb6d18369a2
DHRUVI ASHISHBHAI DHULIA <23dit009@charusat.edu.in>
2024-07-25 21:20:50 +0530

    Add files via upload

---
+++ prac1.java
@@ -0,0 +1,12 @@
+class prac1 {
+    public static void main(String[] args) {
+
+        int age;
+
+        age = 25;
+
+        System.out.println(age + " is the age of Sam.");
+        System.out.println("ID : 23DIT009");
+
+    }
+}
